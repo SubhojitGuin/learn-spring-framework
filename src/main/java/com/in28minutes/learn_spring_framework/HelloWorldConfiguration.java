73f8b0e2d6d0fd7766be06b3ab746609caaf05d7
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-05 22:01:18 +0530

    Add HelloWorldConfiguration with Bean definition and update App02HelloWorldSpring to retrieve the Bean

--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,9 +1,14 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // Configure the things we want Spring to manage - @Configuration
 @Configuration
 public class HelloWorldConfiguration {
 
+    @Bean
+    public String name() {
+        return "Ranga";
+    }
 }
