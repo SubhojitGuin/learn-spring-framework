792a75d44e880d24576c3c53bbae8f02844fa739
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-05 23:41:37 +0530

    Add additional Bean definitions for age, person, and address in HelloWorldConfiguration and update App02HelloWorldSpring to retrieve them

--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,6 +3,9 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) { }
+record Address(String firstLine, String city) { }
+
 // Configure the things we want Spring to manage - @Configuration
 @Configuration
 public class HelloWorldConfiguration {
@@ -10,5 +13,20 @@ public class HelloWorldConfiguration {
     @Bean
     public String name() {
         return "Ranga";
+    }
+
+    @Bean
+    public int age() {
+        return 21;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Ravi", 22);
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("26A South-West Road", "Ohio");
     }
 }
