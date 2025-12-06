44a997f2ddc745e57e7f7c7e0193d78eae44047a
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-06 16:38:48 +0530

    List all the Beans managed by Spring Framework

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -2,6 +2,8 @@ package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         //1: Launch a Spring Context with the HelloWorldConfiguration
@@ -20,5 +22,8 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2")); // Bean name is used to refer to the Bean
         System.out.println(context.getBean(Address.class)); // Bean type is used to refer to the Bean
+
+        // List all Beans managed by Spring Framework
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
